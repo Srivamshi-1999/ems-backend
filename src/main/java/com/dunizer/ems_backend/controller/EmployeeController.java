29adcb2042272619d9bb22d112ec9e0c22a242b2
Srivamshi Alle <alle.srivamshinani@gmail.com>
2025-03-02 18:45:32 +0530

    Employee Rest API

---
+++ src/main/java/com/dunizer/ems_backend/controller/EmployeeController.java
@@ -0,0 +1,59 @@
+package com.dunizer.ems_backend.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dunizer.ems_backend.dto.EmployeeDto;
+import com.dunizer.ems_backend.service.EmployeeService;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/api/employees")
+public class EmployeeController {
+
+    private  EmployeeService employeeService;
+
+    // Build Add Employee REST API
+    @PostMapping
+    public ResponseEntity<?> createEmployee(@RequestBody EmployeeDto employeeDto) {
+
+        EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
+
+        return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
+
+    // Build Get Employee By ID REST API
+    @GetMapping("{id}")
+    public ResponseEntity<?> getEmployeeById(@PathVariable("id") Long employeeId) {
+
+        EmployeeDto employee = employeeService.getEmployeeById(employeeId);
+
+        return new ResponseEntity<>(employee, HttpStatus.OK);
+    }
+
+    // Build Get All Employees REST API
+    @GetMapping
+    public ResponseEntity<?> getAllEmployees() {
+
+        return new ResponseEntity<>(employeeService.getAllEmployees(), HttpStatus.OK);
+    }
+
+    // Build Update Employee REST API
+
+    @PostMapping("{id}")
+    public ResponseEntity<?> updateEmployee(@PathVariable("id") Long employeeId, @RequestBody EmployeeDto employeeDto) {
+
+        EmployeeDto updatedEmployee = employeeService.updateEmployee(employeeId, employeeDto);
+
+        return new ResponseEntity<>(updatedEmployee, HttpStatus.OK);
+    }
+
+}
---
+++ src/main/java/com/dunizer/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,60 @@
+package com.dunizer.ems_backend.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.dunizer.ems_backend.dto.EmployeeDto;
+import com.dunizer.ems_backend.entity.Employee;
+import com.dunizer.ems_backend.mapper.EmployeeMapper;
+import com.dunizer.ems_backend.repository.EmployeeRepository;
+import com.dunizer.ems_backend.service.EmployeeService;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        // TODO Auto-generated method stub
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee savedEmployee = employeeRepository.save(employee);
+        
+        return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        // TODO Auto-generated method stub
+        Employee employee = employeeRepository.findById(employeeId)
+            .orElseThrow(() -> new RuntimeException("Employee not found with given id: " + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        // TODO Auto-generated method stub
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee)).toList();
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto employeeDto) {
+        // TODO Auto-generated method stub
+        Employee employee = employeeRepository.findById(employeeId)
+            .orElseThrow(() -> new RuntimeException("Employee not found with given id: " + employeeId));
+        
+        employee.setFirstName(employeeDto.getFirstName());
+        employee.setLastName(employeeDto.getLastName());
+        employee.setEmail(employeeDto.getEmail());
+        Employee updatedEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployee);
+        
+        
+    }
+
+}
